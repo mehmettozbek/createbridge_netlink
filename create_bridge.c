94ae2908c755a1e8039b261dd5df17a3013a3d02
Mehmet Ozbek <mehmet.ozbek@siemens.com>
2022-09-14 10:32:25 -0400

    ip link set <devicename> master <bridgename> feature added

--- create_bridge.c
+++ create_bridge.c
@@ -1,15 +1,24 @@
 #include <netlink/netlink.h>
 #include <netlink/route/link.h>
 #include <netlink/route/link/bridge.h>
-
+#include <netlink/route/link/bridge_info.h>
 #include <linux/netlink.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <linux/rtnetlink.h>
+#include <libnetlink.h>
+#include <net/if.h>
+
 
 #define TEST_BRIDGE_NAME "br0"
+#define TEST_INTERFACE_NAME "eth0"
+
 
 static int create_bridge(struct nl_sock *sk, struct nl_cache *link_cache, const char *name) {
 	struct rtnl_link *link;
-	int err;
-
+	int err	;
 	link = rtnl_link_alloc();
 	if ((err = rtnl_link_set_type(link, "bridge")) < 0) {
 		rtnl_link_put(link);
@@ -30,6 +39,7 @@ int main()
 	struct rtnl_link *link;
 	struct nl_cache *link_cache;
 	struct nl_sock *sk;
+	struct rtnl_link *ltap;
 	int err;
 
 	sk = nl_socket_alloc();
@@ -49,15 +59,34 @@ int main()
 	}
 
 	nl_cache_refill(sk, link_cache);
+
 	link = rtnl_link_get_by_name(link_cache, TEST_BRIDGE_NAME);
-	
+	ltap = rtnl_link_get_by_name(link_cache, TEST_INTERFACE_NAME);
+	if (!ltap) {
+		fprintf(stderr, "You should create a tap interface before lunch this test (# tunctl -t %s)\n", TEST_INTERFACE_NAME);
+		return -1;
+	}
+
+	if ((err = rtnl_link_enslave(sk, link, ltap)) < 0) {
+		nl_perror(err, "Unable to enslave interface to his bridge\n");
+		return err;
+	}
 
 	if(rtnl_link_is_bridge(link) == 0) {
 		fprintf(stderr, "Link is not a bridge\n");
 		return -2;
 	}
 
+	rtnl_link_put(ltap);
 	nl_cache_refill(sk, link_cache);
+	ltap = rtnl_link_get_by_name(link_cache, TEST_INTERFACE_NAME);
+
+	if(rtnl_link_get_master(ltap) <= 0) {
+		fprintf(stderr, "Interface is not attached to a bridge\n");
+		return -3;
+	}
+
+	rtnl_link_put(ltap);
 	rtnl_link_put(link);
 
 	nl_cache_free(link_cache);
